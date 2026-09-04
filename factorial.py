ce5bfd9065352e4823ee5ec02c39b9eb8ed094ea
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-09-04 10:40:49 +0600

    python

--- factorial.py
+++ factorial.py
@@ -1,2 +1,11 @@
 def factorial(n):
-    
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
+n = int(input("Enter a positive number:"))
+if n<0:
+    print("You enterd a negative number so try again!")
+    exit(0)
+else:
+    print("The factorical of ",n,"is:", factorial(n))
