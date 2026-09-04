c690baf9baa62d8b38a7aacf501ec4eca8147d3b
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-09-04 10:24:48 +0600

    first commit

---
+++ area.py
@@ -0,0 +1,7 @@
+def Area():
+    length = float(input("Enter Length:"))
+    width = float(input("Enter Width:"))
+    RectangleArea = length * width
+    print("The area of rectangle is:", RectangleArea)
+
+Area()
