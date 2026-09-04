6d1d96e540e71e78ce41aa52ffdea6837f6e0d4c
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-09-04 11:01:01 +0600

    python

--- triangle.py
+++ triangle.py
@@ -1,2 +1,6 @@
 # Python program to calculate the area of ​​a triangle using functions
 
+import math
+def Triangle():
+    a = int(input("Enter the value of a :"))
+    b = int(input("Enter the value of b :")) 
