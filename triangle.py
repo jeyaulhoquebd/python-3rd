7e13c62904de87b1b2f821a8bf89f2db494feac2
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-09-04 11:08:43 +0600

    python

--- triangle.py
+++ triangle.py
@@ -1,6 +1,24 @@
 # Python program to calculate the area of ​​a triangle using functions
 
 import math
+
 def Triangle():
     a = int(input("Enter the value of a :"))
     b = int(input("Enter the value of b :")) 
+    c = int(input("Enter the value of c :"))
+
+    if((a+b)>c and (b+c)>a and (a+c)>a):
+
+        s = (a+b+c)/2
+
+        Area = math.sqrt(s*(s-a)*(s-b)*(s-c))
+
+        print("The area of this Triangle is :", Area)
+
+    else:
+
+        print("Triangle is not posslible")
+
+    Triangle()
+
+    
