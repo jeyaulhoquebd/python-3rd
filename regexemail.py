b53b797437f8687f104ebbef715d0e02d243bf99
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-09-05 14:55:20 +0600

    python

--- regexemail.py
+++ regexemail.py
@@ -1,2 +1,9 @@
 # Python program to verify email address using RegEx
 
+import re
+patt = "[a-zA=Z0-9]+@[a-zA-Z]+\.(com|net|in)"
+def isValid(email):
+    if re.search(patt, email):
+        print("Email is valid")
+    else:
+        print("Email is Invalid")
