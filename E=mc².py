fbc8d18542f7fe03e87e2aacb66253b160f6efe2
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-09-06 01:04:06 +0600

    python

--- E=mc².py
+++ E=mc².py
@@ -5,3 +5,18 @@ def calculate_enery(mass):
     mass : mass in kilograms
     returns : energy in joules
     """ 
+    c = 299792458
+    energy = mass * (c ** 2)
+    return energy
+
+mass = 1
+E = calculate_enery(mass)
+
+print(f"Mass : {mass} kg")
+
+print (f"Speed of light : {299792458} m/s")
+
+print(f"Energy (E): {E:,} joules")
+
+print(f"Energy (E): {E/1e15:.2f} petajoules")
+
